bb73146889e7e4a62558ef6c5ff13c158280cfa8
longheeKwan <kavinkwan@foxmail.com>
2021-12-27 17:42:03 +0800

    update

---
+++ 46.全排列.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=46 lang=javascript
+ *
+ * [46] 全排列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    const visited = []
+    const res = []
+    const track = (nums, visited) => {
+        if (visited.length === nums.length) {
+            res.push(JSON.parse(JSON.stringify(visited)))
+            return
+        }
+        for (let i = 0; i < nums.length; i++) {
+            if (visited.includes(nums[i])) continue
+            visited.push(nums[i])
+            track(nums, visited)
+            visited.pop()
+        }
+    }
+    track(nums, visited)
+    return res
+
+};
+
+// @lc code=end
+
