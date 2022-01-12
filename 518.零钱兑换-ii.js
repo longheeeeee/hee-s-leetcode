958e5e3aaff72a3e64029bd0a6bf092d3c38b100
longheeKwan <kavinkwan@foxmail.com>
2022-01-12 20:58:03 +0800

    update

--- 518.零钱兑换-ii.js
+++ 518.零钱兑换-ii.js
@@ -10,31 +10,6 @@
  * @param {number[]} coins
  * @return {number}
  */
-// var change = function(amount, coins) {
-//   // dp[i]代表了使用硬币组合成总价i的组合数
-//   // 初始值为：dp[0]代表组合成0元的组合数，dp[0]=1
-//   // dp[i] = sum(dp[i-coin])
-//   const dp = new Array(amount + 1).fill(0)
-//   dp[0] = 1
-//   for (let i = 1; i < amount+1; i++) {
-//     let sum = 0
-//     for (let c = 0; c < coins.length; c++) {
-//       (i - coins[c] >= 0) && (sum += dp[i - coins[c]])
-//     }
-//     dp[i] = sum
-//   }
-//   console.log('dp', dp)
-//   return dp[amount]
-// }
-
-
-
-
-
-
-
-
-
 
 // var change = function(amount, coins) {
 //   // dp[i][j]代表了，使用i个硬币能凑成金额j的组合数为dp[i][j]
@@ -76,19 +51,51 @@
 // }
 
 
+// 优化方法：因为dp[i][j]的前置数的索引都小于 i  j，所以只需要维护一个数组
+// var change = function(amount, coins) {
+//   const dp = []
+//   for (let i = 0; i <= amount; i++) {
+//     dp[i] = 0
+//   }
+//   dp[0] = 1
+//   for (let i = 1; i <= coins.length; i++) {
+//     const coin = coins[i - 1]
+//     for (let j = 0; j < amount + 1; j++) {
+//       dp[j] = dp[j] + (j - coin < 0 ? 0 : dp[j - coin])
+//     }
+//   }
+//   return dp[amount]
+// }
+
+
+// 2022-01-12 复习：
+
+// 先作dp数组的定义：
+// dp[i][j]为：使用第i个硬币组合成金额j的组合数
+// 其中：i为0表示不使用硬币，所以第i个硬币为coins[i-1]即coin
+
+// dp[i][j] = dp[i-1][j] + dp[i][j-coin] (j-coin > 0)
+
+// 状态转移方程分为两种情况：
+// 1.不使用第i个硬币：使用第i-1个硬币组合出金额为j的组合数
+// 2.使用第i个硬币：使用第i个硬币组合出金额为j-coin的组合数
+// 其中：当j-coin<0时组合数为0
+
+// 边界条件：
+// 当i==0时，不使用硬币组合出金额的组合数为0，dp[0][j] = 0
+// 当j==0时，使用各种硬币都只有一种组合，dp[i][0] = 1
 var change = function(amount, coins) {
   const dp = []
-  for (let i = 0; i <= amount; i++) {
-    dp[i] = 0
+  for (let index = 0; index <= coins.length; index++) {
+    dp[index] = new Array(amount+1).fill(0)
+    dp[index][0] = 1
   }
-  dp[0] = 1
   for (let i = 1; i <= coins.length; i++) {
-    const coin = coins[i - 1]
-    for (let j = 0; j < amount + 1; j++) {
-      dp[j] = dp[j] + (j - coin < 0 ? 0 : dp[j - coin])
+    for (let j = 0; j <= amount; j++) {
+      dp[i][j] = dp[i-1][j] + ((j-coins[i-1] >= 0) ? dp[i][j-coins[i-1]] : 0)
     }
   }
-  return dp[amount]
+  return dp[coins.length][amount]
 }
 // @lc code=end
 
