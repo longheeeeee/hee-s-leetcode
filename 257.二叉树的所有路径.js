59ee2ec2f1779ff08dff7975f338371cef99220a
NgWingLam <ng.winglam@qq.com>
2024-07-02 00:51:22 +0800

    1

---
+++ 257.二叉树的所有路径.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=257 lang=javascript
+ *
+ * [257] 二叉树的所有路径
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+  const res = []
+  const dfs = (node, path) => {
+    if (!node.left && !node.right) {
+      res.push([...path, node.val].join('->'))
+      return
+    }
+    if (node.left) {
+      dfs(node.left, [...path, node.val])
+    }
+    if (node.right) {
+      dfs(node.right, [...path, node.val])
+    }
+  }
+  dfs(root, [])
+  return res
+};
+// @lc code=end
+
